04470017f97a1034ee86bcaa5a897115163813ec
nelcolon <ncolonmeran@gmail.com>
2022-09-25 21:42:15 -0400

    adding completed project

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,20 +2,36 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Nelson Colon
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+           // Code you want to be delayed
+            sender.alpha = 1
+        }
+        let title: String = (sender.titleLabel?.text)!
+        playSound(letter: title)
+    }
     
-
+    func playSound(letter: String) {
+        let url = Bundle.main.url(forResource: letter, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
